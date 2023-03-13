6575829a181f84af6fe48071f23f4b9060e01901
Julio Leon <juliopena143@icloud.com>
2023-03-12 19:08:09 -0600

    counter and setCounter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
-const App = () => {
 
-  const name = 'John';
-  const isNameShowing = true;
+const App = () => {
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
-      <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }
